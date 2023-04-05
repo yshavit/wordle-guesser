83177845fcb979bfdd2b7b1d00a8c0008416d20f
Yuval Shavit <yankee.sierra@gmail.com>
2023-04-05 03:33:56 -0400

    derp, don't want the dummy data :)

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,25 @@ use wordlehelper::window_helper;
 use wordlehelper::word_list::WordList;
 
 fn main() {
-    // let window = initscr();
-    // window_helper::init(&window);
-    //
-    // let mut guess_grid = GuessGrid::<5, 6>::new();
-    // guess_grid.draw(&window);
-    //
-    // window.refresh();
-    // loop {
-    //     match window.getch() {
-    //         Some(Input::Character(c)) if c == '\t' => break,
-    //         Some(input) => guess_grid.handle_input(&window, input),
-    //         _ => {}
-    //     }
-    //     guess_grid.draw(&window);
-    //     window.refresh();
-    // }
-    // endwin();
+    let window = initscr();
+    window_helper::init(&window);
 
-    let guess_grid = GuessGrid::generate_dummy_data();
+    let mut guess_grid = GuessGrid::<5, 6>::new();
+    guess_grid.draw(&window);
+
+    window.refresh();
+    loop {
+        match window.getch() {
+            Some(Input::Character(c)) if c == '\t' => break,
+            Some(input) => guess_grid.handle_input(&window, input),
+            _ => {}
+        }
+        guess_grid.draw(&window);
+        window.refresh();
+    }
+    endwin();
+
+    // let guess_grid = GuessGrid::generate_dummy_data();
 
     let knowledge = GridKnowledge::from_grid(&guess_grid);
     println!("one");
